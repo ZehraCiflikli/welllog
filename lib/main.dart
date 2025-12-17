eb494b5097cb86fc27abcb7129f3afe7ff6b8c8b
Berivan <berivanalpagu@gmail.com>
2025-12-17 20:21:42 +0300

    main.dart fix

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initializeDateFormatting('tr'); // 👈 şart
+  await initializeDateFormatting('tr');
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(
